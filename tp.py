78205b379e2f48f60b3e5dbe0cadfc5c6683906d
bandiaoz <893396998@qq.com>
2022-10-17 18:26:45 +0800

    update

--- tp.py
+++ tp.py
@@ -4,8 +4,6 @@ def get_files():
     for filepath,dirnames,filenames in os.walk(r'.'):
         for filename in filenames:
             if filename.split('.')[-1] in ['aux', 'log', 'out', 'gz', 'DS_Store']:
-                # print("git rm -r --cached" + filepath + '/' + filename)
                 os.system("git rm -r --cached " + filepath + '/' + filename)
-        # print(filepath, '\n')
 
 get_files()
